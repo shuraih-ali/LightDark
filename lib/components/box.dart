284d3c85fdb28473c29daa454a671ecebaba199a
shuraih-ali <shuraihali003@gmail.com>
2025-08-27 09:24:33 +0500

    Just Tap!

---
+++ lib/components/box.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyBox extends StatelessWidget {
+  final Widget? child;
+  final Color? color;
+  const MyBox({super.key,required this.child, required this.color
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      width: 200,
+      height: 200,
+      padding: EdgeInsets.all(50),
+      child: child,
+    );
+  }
+}
